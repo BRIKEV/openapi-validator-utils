d04a4cf72ea9355d66f36627bb2a97d8203eae47
Kevin Julián Martínez Escobar <kevinccbsg@gmail.com>
2021-04-28 00:43:25 +0200

    Fix/path validation message (#41)
    * feat: update params tests
    
    * fix: sanitize method
    
    * chore: bump version to 1.1.3

--- utils/sanitizeValueSchema.js
+++ utils/sanitizeValueSchema.js
@@ -3,12 +3,17 @@ const numberTypes = [
   'integer',
 ];
 
+const booleanTypes = [
+  'true',
+  'false',
+];
+
 const sanitizeValueSchema = (value, schema) => {
-  if (numberTypes.includes(schema.type)) {
+  if (numberTypes.includes(schema.type) && !Number.isNaN(Number(value))) {
     return Number(value);
   }
-  if (schema.type === 'boolean') {
-    return schema.type === 'true';
+  if (schema.type === 'boolean' && booleanTypes.includes(value)) {
+    return value === 'true';
   }
   return value;
 };
